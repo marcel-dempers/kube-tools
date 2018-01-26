d4f028cdc7f7228edb9bb3933727b14ed2660747
Chorn Sokun <sokun.chorn@webjet.com.au>
2018-01-26 14:19:34 +1100

    a better way to kubetools

---
+++ kubetools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CONTAINER_NAME=kubetools
+
+if [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME 2> /dev/null )" == "" ]; then
+    docker run -it --name $CONTAINER_NAME       \
+        -v ~/.azure:/root/.azure                \
+        -v $PWD:/var/lib/src                   \
+        -v ~/.kube/config:/root/.kube/config    \
+        --rm -p 8001:8001                       \
+        --workdir /var/lib/src                  \
+        aimvector/kube-tools:latest
+else
+    docker exec -it $CONTAINER_NAME bash
+fi
