f077a17816662fde8f28314e93d37d4c8618449d
marcel-dempers <aimvec@gmail.com>
2018-06-14 08:09:57 +1000

    make colors as variables so it can be configured

--- kubeps1.sh
+++ kubeps1.sh
@@ -6,12 +6,15 @@ parse_git_branch() {
 my_kube_ps1() {  
     source <(kubectl completion bash)
     KUBE_PS1_SYMBOL_COLOR=blue
-    KUBE_PS1_CTX_COLOR=red
-    KUBE_PS1_NS_COLOR=green
+    KUBE_PS1_CTX_COLOR="48;5;4m"
+    KUBE_PS1_NS_COLOR="48;5;4m"
     KUBE_PS1_BG_COLOR=''
     KUBE_PS1_SYMBOL="⎈"
+    KUBE_PS1_USER_COLOR="48;5;4m"
+    KUBE_PS1_FOLDER_COLOR="48;5;242m"
+    KUBE_PS1_GIT_BRANCH_COLOR="0;33;49m"
     source ~/kube-ps1/kube-ps1.sh
-    PS1='\[\e[0;30;41m\][\u@\h]\[\e[0;0m\]\[\e[0;37;100m\][\W]\[\e[0;0m\] \[\e[0;33;49m\]$(parse_git_branch)\[\e[0;0m\] $(kube_ps1)\n'
+    PS1='\[\e[${KUBE_PS1_USER_COLOR}\][\u@\h]\[\e[0;0m\]\[\e[${KUBE_PS1_FOLDER_COLOR}\][\W]\[\e[0;0m\] \[\e[${KUBE_PS1_GIT_BRANCH_COLOR}\]$(parse_git_branch)\[\e[0;0m\] $(kube_ps1)\n'
     PS1+='  |--> '
     export PS1
 }
